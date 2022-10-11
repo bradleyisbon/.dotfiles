41d8fead0e7ebbb12b3eae503892a67fe7a8d232
Bradley Isbon <bradisbon@gmail.com>
2022-10-11 14:59:24 -0500

    add javascript to ts shiftwidth autocmd

--- nvim/after/plugin/autocmds.lua
+++ nvim/after/plugin/autocmds.lua
@@ -14,6 +14,6 @@ api.nvim_create_autocmd("TextYankPost", {
 
 -- Set shiftwidth for typescript
 api.nvim_create_autocmd("FileType", {
-	pattern = {"typescript"},
+	pattern = {"typescript", "javascript"},
 	command = "set shiftwidth=4"
 })
