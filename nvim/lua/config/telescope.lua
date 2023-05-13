77afccf51d32ab85103443b615c60a7abbbe1107
Bradley Isbon <bradisbon@gmail.com>
2023-05-12 21:00:30 -0500

    chore: Remove dup mapping

--- nvim/lua/config/telescope.lua
+++ nvim/lua/config/telescope.lua
@@ -11,7 +11,6 @@ function M.setup()
 		f = {
 			name = "Telescope",
 			a = {"<cmd>Telescope<cr>", "All Pickers"},
-			f = {"<cmd>Telescope find_files<cr>", "Find Files"},
 			g = {"<cmd>Telescope live_grep<cr>", "Live Grep"},
 			b = {"<cmd>Telescope buffers<cr>", "Buffers"},
 			h = {"<cmd>Telescope help_tags<cr>", "Help Tags"},
