8005e43b19ff86f8c66e808cfbe835ff2376c28f
Bradley Isbon <bradisbon@gmail.com>
2023-10-08 01:00:46 -0500

    chore: Rename

--- nvim/lua/config/luasnip.lua
+++ nvim/lua/config/luasnip.lua
@@ -1,12 +1,13 @@
 local M = {}
 
 function M.setup()
-	local luasnip = require "luasnip"
+	local ls = require "luasnip"
 
-	luasnip.config.set_config {
+	ls.config.set_config {
 		history = false,
 		updateevents = "TextChanged,TextChangedI"
 	}
+
 
 	require("luasnip/loaders/from_vscode").load()
 end
