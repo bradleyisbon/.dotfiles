c0ac540d47f2a206676395ff2dff79163f27d017
Bradley Isbon <bradisbon@gmail.com>
2025-01-05 12:16:21 -0600

    chore: Comment out yaml prettier formatter

--- nvim/lua/config/formatter.lua
+++ nvim/lua/config/formatter.lua
@@ -3,9 +3,10 @@ local M = {}
 function M.setup()
 	require('formatter').setup {
 		filetype = {
-			yaml = {
-				require('formatter.filetypes.yaml').prettier
-			}
+			-- removing temporarily because yaml bad
+			-- yaml = {
+			-- 	require('formatter.filetypes.yaml').prettier
+			-- }
 		}
 	}
 end
